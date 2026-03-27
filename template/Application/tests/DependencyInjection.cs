1a3ffbcef69cb0b447d001311d529a915e68719a
Xavier <1859710+xavierjohn@users.noreply.github.com>
2026-03-27 14:58:00 -0700

    Change repository registration from singleton to scoped for TodoItem and ITodoRepository (#37)

--- template/Application/tests/DependencyInjection.cs
+++ template/Application/tests/DependencyInjection.cs
@@ -15,8 +15,8 @@ public static class DependencyInjection
         var actorProvider = new TestActorProvider("test-user", Permissions.TodosCreate, Permissions.TodosRead, Permissions.TodosUpdate, Permissions.TodosComplete, Permissions.TodosDelete);
         services.AddSingleton<TestActorProvider>(actorProvider);
         services.AddSingleton<IActorProvider>(actorProvider);
-        services.AddSingleton<FakeRepository<TodoItem, TodoId>>();
-        services.AddSingleton<ITodoRepository, FakeRepositoryAdapter>();
+        services.AddScoped<FakeRepository<TodoItem, TodoId>>();
+        services.AddScoped<ITodoRepository, FakeRepositoryAdapter>();
         services.AddResourceAuthorization(typeof(CompleteTodoCommand).Assembly, typeof(FakeCompleteTodoResourceLoader).Assembly);
         return services;
     }
